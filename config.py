4d335223ba1a346b86330a247424610cd11c5096
gecosta4i20 <genivanfc@bol.com.br>
2017-07-07 01:02:27 -0300

    commit inicial

---
+++ config.py
@@ -0,0 +1,33 @@
+
+# Ports for your workers
+STRATUM_HOST = "127.0.0.1"
+STRATUM_PORT = 8080
+
+# Coin address where money goes. If you mine direct to the exchange, you MUST specify payment_id together with wallet of exchange.
+WALLET = '43c2ykU9i2KZHjV8dWff9HKurYYRkckLueYK96Qh4p1EDoEvdo8mpgNJJpPuods53PM6wNzmj4K2D1V11wvXsy9LMiaYc86'
+# Only if you mine direct to the exchange
+PAYMENT_ID = ''
+
+# It's useful for individually monitoring and statistic.
+# In your workers you have to use any number as username (without wallet!)
+ENABLE_WORKER_ID = True
+WORKER_ID_FROM_IP = False
+
+# On DwarfPool you have option to monitor your workers via email.
+# If WORKER_ID is enabled, you can monitor every worker/rig separately.
+MONITORING = True
+MONITORING_EMAIL = 'genivanfc@gmail.com'
+
+# Main pool
+POOL_HOST = 'xmr-eu.dwarfpool.com'
+POOL_PORT = 8050
+
+# Failover pool
+POOL_FAILOVER_ENABLE = False
+POOL_HOST_FAILOVER = 'xmr-usa.dwarfpool.com'
+POOL_PORT_FAILOVER = 8050
+
+# ERROR, INFO, DEBUG
+LOGLEVEL = 'INFO'
+DEBUG = False
+LOGFILE = "logfile.log"
